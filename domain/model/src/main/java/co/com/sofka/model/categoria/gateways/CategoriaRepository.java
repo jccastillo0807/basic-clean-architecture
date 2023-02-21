ca1578638526e4600296a6a05a953c931410cca0
jose.castillo@sofka.com.co <jose.castillo@sofka.com.co>
2023-02-21 10:30:32 -0500

    primer commit

---
+++ domain/model/src/main/java/co/com/sofka/model/categoria/gateways/CategoriaRepository.java
@@ -0,0 +1,4 @@
+package co.com.sofka.model.categoria.gateways;
+
+public interface CategoriaRepository {
+}
---
+++ domain/model/src/main/java/co/com/sofka/model/producto/gateways/ProductoRepository.java
@@ -0,0 +1,4 @@
+package co.com.sofka.model.producto.gateways;
+
+public interface ProductoRepository {
+}
